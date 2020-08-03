1630dbe41d52490ce1f42dbf627a6add16b5fb93
Kevin Yang <phyang018@gmail.com>
2020-08-03 12:07:00 -0700

    Updates

---
+++ precache-manifest.6478fb886accf773127887710c5158f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4165d00400e9da32d374aebf9e04bf81",
+    "url": "/moviereactapp/index.html"
+  },
+  {
+    "revision": "9430ed7328e6e05ee0ea",
+    "url": "/moviereactapp/static/css/main.3f238148.chunk.css"
+  },
+  {
+    "revision": "afe5b35d28a894d24e76",
+    "url": "/moviereactapp/static/js/2.1df69fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviereactapp/static/js/2.1df69fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9430ed7328e6e05ee0ea",
+    "url": "/moviereactapp/static/js/main.fef67d35.chunk.js"
+  },
+  {
+    "revision": "4d7fbe50ea0e6cfbc12e",
+    "url": "/moviereactapp/static/js/runtime-main.d9f12503.js"
+  }
+]);
